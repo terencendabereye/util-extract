525c006079e681ff720934e95d0e093ef8b9dfcd
Terence Ndabereye <ndabereye@gmail.com>
2023-11-17 14:38:27 +0200

    added version command

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import "extract/cmd"
+import (
+	"extract/cmd"
+
+	"os"
+)
 
 // import "cmd"
+var (
+	version string
+)
 
 func main() {
+	os.Setenv("VERSION_NUMBER", version)
 	cmd.Extract()
+	// fmt.Printf("version=%s build=%s\n", version, date)
 }
